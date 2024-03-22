e86106ff550e3162d0b7a99fcf7b508c76f69d20
alifinrayhan <160594805+alifinrayhan@users.noreply.github.com>
2024-03-22 17:32:47 +0700

    Add files via upload

---
+++ UCPSorting_0091/UCP 1_Algoritma Struktur Data/UCP 1_Algoritma Struktur Data.cpp
@@ -0,0 +1,43 @@
+// 1. Cara InsertionSort membandingkan serta menukar elemen-elemen dengan, ketika pass 1 angka pertama di bandingkan dengan angka sebelahnya ketika angka sebelahnya lebih besar maka di tuker 
+// 2. Cara SelectionSort Membandingkan serta menukar elemen-elemen dengan, set pass 1
+// 3. Cara mengetahui Jumlah Langkah Yang di lakukan dalam sortir
+
+//4. 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int alifin[20];
+int n,i;
+
+void input() {
+
+	while (true) {
+
+		cout << "Masukkan banyaknya elemen alifin : ";
+		cin >> n;
+		if (n <= 20) {
+			break;
+
+		}
+		else {
+
+			cout << "\nArray dapat mempunyai maksimal 20 elemen \n";
+
+
+		}
+
+		cout << endl;
+		cout << "============" << endl;
+		cout << "Masukan Elemen Array" << endl;
+		cout << "============" << endl;
+
+		for (int i = 0; i < n; i++) {
+			cout << "Data ke-" << ()
+		}
+	}   
+
+}    
+
+
+
